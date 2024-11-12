e42f4c598facdaca4ad9514ff7cc9fe3f4d040b2
ValenDadamo <valentindadamo@gmail.com>
2024-11-12 01:24:53 -0300

    fix: Vagrantfile | provision.sh | compartido

--- VM/provision.sh
+++ VM/provision.sh
@@ -2,16 +2,13 @@
 
 # Actualizar y configurar la máquina
 if [ -f /etc/debian_version ]; then
-  # Ubuntu
-  sudo apt-get update -y
-  sudo apt-get install -y lvm2 ansible docker.io tree git openssh-server sshpass
-  sudo ufw disable
+    # Ubuntu
+    sudo apt-get update -y
+    sudo apt-get install -y lvm2 ansible docker.io tree git
 elif [ -f /etc/fedora-release ]; then
-  # Fedora
-  sudo dnf update -y
-  sudo dnf install -y lvm2 ansible docker tree git openssh-server sshpass
-  sudo systemctl stop firewalld
-  sudo systemctl disable firewalld
+    # Fedora
+    sudo dnf update -y
+    sudo dnf install -y lvm2 ansible docker tree git
 fi
 
 # Configuración de SSH sin contraseña
@@ -25,13 +22,5 @@ chmod 600 /home/vagrant/.ssh/authorized_keys
 echo 'vagrant ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers > /dev/null
 
 # Agregar referencias a /etc/hosts para la resolución de nombres
-echo "192.168.56.10 testing" | sudo tee -a /etc/hosts > /dev/null
-echo "192.168.56.11 production" | sudo tee -a /etc/hosts > /dev/null
-
-# Cruzar las claves SSH entre las máquinas
-if [ "$(hostname)" == "testing" ]; then
-  sshpass -p 'vagrant' ssh-copy-id -o StrictHostKeyChecking=no vagrant@192.168.56.11 || echo "No se pudo copiar la clave a production"
-elif [ "$(hostname)" == "production" ]; then
-  sshpass -p 'vagrant' ssh-copy-id -o StrictHostKeyChecking=no vagrant@192.168.56.10 || echo "No se pudo copiar la clave a testing"
-fi
-
+echo "192.168.56.1 test" | sudo tee -a /etc/hosts > /dev/null
+echo "192.168.56.2 prod" | sudo tee -a /etc/hosts > /dev/null
