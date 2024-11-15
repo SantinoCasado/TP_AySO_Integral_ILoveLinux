95262c13473462856acc66ab820ce6b9a6145cfc
ValenDadamo <valentindadamo@gmail.com>
2024-11-15 00:41:08 -0300

    fix: all

--- VM/provision.sh
+++ VM/provision.sh
@@ -12,13 +12,13 @@ elif [ -f /etc/fedora-release ]; then
 fi
 
 # Configuración de SSH sin contraseña
-mkdir -p $HOME/.ssh
-ssh-keygen -t ed25519 -N "" -f $HOME/.ssh/id_ed25519
-cat $HOME/.ssh/id_ed25519.pub >> /home/vagrant/.ssh/authorized_keys
+# mkdir -p $HOME/.ssh
+# ssh-keygen -t ed25519 -N "" -f $HOME/.ssh/id_ed25519
+# cat $HOME/.ssh/id_ed25519.pub >> /home/vagrant/.ssh/authorized_keys
 
 # Configurar sudo sin contraseña
-echo "ALL ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/nopasswd_sudo > /dev/null
+#echo "ALL ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/nopasswd_sudo > /dev/null
 
 # Agregar referencias a /etc/hosts para la resolución de nombres
-echo "192.168.56.10 test" | sudo tee -a /etc/hosts > /dev/null
-echo "192.168.56.11 prod" | sudo tee -a /etc/hosts > /dev/null
+echo "192.168.56.4 test" | sudo tee -a /etc/hosts > /dev/null
+echo "192.168.56.5 prod" | sudo tee -a /etc/hosts > /dev/null
